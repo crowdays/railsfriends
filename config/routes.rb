b071d08c0683339cef3c45634251cb95ffed611a
Lorenzo Demario Juarez <126051157+crowdays@users.noreply.github.com>
2023-03-03 13:44:44 -0600

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
+
+  resources :friends
+  #get 'home/index'
+  get 'home/about'
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_040935) do
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
